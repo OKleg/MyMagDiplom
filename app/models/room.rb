5699268c980a1d8ce7c1c4911b1e4037ea347719
Олег <57410308+OKleg@users.noreply.github.com>
2025-01-31 23:45:31 +0300

    fix

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
+  has_rich_text :content
+
   validates_uniqueness_of :name
   scope :public_rooms, -> {where(is_private: false)}
   after_create_commit { broadcast_append_to 'rooms' }
